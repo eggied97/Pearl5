36daab101e18fa2a944b2f226dbfaefc9010f86d
Egbert Dijkstra <egbert@edeveloping.nl>
2015-10-09 12:28:46 +0200

    -added that the qeue would be emptied at the end

--- bonus.py
+++ bonus.py
@@ -8,50 +8,48 @@ queue = []
 timeQueue = []
 waitTime = []
 
-initArray = 0
-maxArrayCount = 100000
+packetCount = 100000
 
-chancePcIsSending = 0.2
+chancePcIsSending = 0.25
 
 countPerTimeCycle = 0
 
-while initArray <= maxArrayCount:
-    packets.append(initArray)
-    initArray += 1
+timePerTick = 0.0001
 
-while len(packets) > 0:
+
+while packetCount > 0:
     pc1IsSending = random.random() < chancePcIsSending
     pc2IsSending = random.random() < chancePcIsSending
     pc3IsSending = random.random() < chancePcIsSending
     pc4IsSending = random.random() < chancePcIsSending
 
     if pc1IsSending:
-        if len(packets) > 0:
+        if packetCount > 0:
             queue.append("pc1")
-            timeQueue.append(time.time())
+            timeQueue.append(time.clock())
             countPerTimeCycle += 1
-            del packets[-1]
+            packetCount -= 1
 
     if pc2IsSending:
-        if len(packets) > 0:
+        if packetCount > 0:
             queue.append("pc2")
-            timeQueue.append(time.time())
+            timeQueue.append(time.clock())
             countPerTimeCycle += 1
-            del packets[-1]
+            packetCount -= 1
 
     if pc3IsSending:
-        if len(packets) > 0:
+        if packetCount > 0:
             queue.append("pc3")
-            timeQueue.append(time.time())
+            timeQueue.append(time.clock())
             countPerTimeCycle += 1
-            del packets[-1]
+            packetCount -= 1
 
     if pc4IsSending:
-        if len(packets) > 0:
+        if packetCount > 0:
             queue.append("pc4")
-            timeQueue.append(time.time())
+            timeQueue.append(time.clock())
             countPerTimeCycle += 1
-            del packets[-1]
+            packetCount -= 1
 
     if (countPerTimeCycle == 1) & (len(queue) == 1):
         #print("{} sended, no waiting time because he was the only one.".format(queue[0]))
@@ -64,7 +62,7 @@ while len(packets) > 0:
 
     else:
         #grab from the queue
-        mWaitTime = time.time() - timeQueue[0]
+        mWaitTime = time.clock() - timeQueue[0]
         #print("{} sended, waited {} seconds.".format(queue[0], mWaitTime))
         waitTime.append(mWaitTime)
         del queue[0]
@@ -72,15 +70,27 @@ while len(packets) > 0:
 
 
     countPerTimeCycle = 0;
-    time.sleep(0.0001)
+    time.sleep(timePerTick)
+
+#empty the queue, if we have any
+while len(queue) > 0:
+
+    mWaitTime = time.clock() - timeQueue[0]
+    waitTime.append(mWaitTime)
+    del queue[0]
+    del timeQueue[0]
+    time.sleep(timePerTick)
+
 
 #now print avarege waiting time:
 sum = 0;
 count = len(waitTime)
 
-while len(waitTime) != 0:
+while len(waitTime) > 0:
     sum += waitTime[0]
     del waitTime[0]
+
+
 
 avarageWaitingTime = sum / count
 
